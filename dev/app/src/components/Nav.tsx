c38cd8005b6c18ff4a3833aca641efe976655712
Alexis <alexis@provost.cloud>
2023-02-20 16:35:15 -0500

    Added nav with icons

--- dev/app/src/components/Nav.tsx
+++ dev/app/src/components/Nav.tsx
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
+import { FaWallet, FaUserAlt } from "react-icons/fa";
 
 const Nav = () => {
 	return (
 		<nav>
+			<div className="branding">
+				<Link to="/">
+					<h1>FF</h1>
+				</Link>
+			</div>
 			<ul>
 				<li>
-					<Link to="/">
-						Accueil
+					<Link to="/dashboard">
+						<FaWallet />
 					</Link>
 				</li>
 				<li>
 					<Link to="/login">
-						Login
+						<FaUserAlt />
 					</Link>
 				</li>
 			</ul>
