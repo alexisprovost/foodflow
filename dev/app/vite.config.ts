bea6018babe307558c9ddfd19ed3a2a12ffbbeed
Alexis <alexis@provost.cloud>
2023-02-23 17:21:51 -0500

    Added updated UML

--- dev/app/vite.config.ts
+++ dev/app/vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: command === 'build' ? '/dev/app/' : '/',
-  publicDir: command === 'build' ? false : 'src/assets',
-
   plugins: [react()],
 })
