1d9a41f8b1f09bbda9350716d601242e4c806689
Alexis Provost <alexis@provost.cloud>
2023-03-27 09:43:03 -0400

    Added working stack for dev env and prod with docker

--- dev/vite-app/vite.config.ts
+++ dev/vite-app/vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	
+	server: {
+		https: true,
+		hmr: {
+			host: "localhost",
+		},
+		host: "0.0.0.0",
+		port: 5173,
+		proxy: {
+			"/api": {
+				target: "http://api:3000",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+		watch: {
+			usePolling: true,
+		},
+	},
+	plugins: [react(), mkcert()],
 });
