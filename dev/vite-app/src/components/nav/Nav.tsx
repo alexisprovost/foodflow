aa0b3da47fc16534bf19e00b2135ae9c27258db9
Alexis Provost <alexis@provost.cloud>
2023-04-12 17:30:43 -0400

    Updated color trend to match 2023 best dark theme styles

--- dev/vite-app/src/components/nav/Nav.tsx
+++ dev/vite-app/src/components/nav/Nav.tsx
@@ -13,7 +13,7 @@ interface NavProps {
 
 const Nav = ({ navItems }: NavProps) => {
 	return (
-		<nav className="bg-navBg top-1/2 transform -translate-y-1/2 h-3/4 w-24 fixed left-0 flex flex-col items-center justify-between p-4 rounded-tr-[2rem] rounded-br-[2rem] shadow-lg">
+		<nav className="bg-secondary top-1/2 transform -translate-y-1/2 h-3/4 w-24 fixed left-0 flex flex-col items-center justify-between p-4 rounded-tr-[2rem] rounded-br-[2rem] shadow-lg">
 			<Link to="/" className="font-fugazone">
 				<h1 className="my-2 text-2xl">FF</h1>
 			</Link>
--- dev/vite-app/tailwind.config.cjs
+++ dev/vite-app/tailwind.config.cjs
@@ -1,32 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    screens: {
-      'sm': '300px',
-      'md': '600px',
-      'lg': {'min': '800px', 'max': '1279px'},
-      'xl': '1280px',
-    },
-    extend: {
-      colors: {
-        primary: "#263238",
-        secondary: "#2c4048",
-        navBg: "#1e272c",
-        primaryText: "#ffffff",
-        secondaryText: "#949494",
-        primaryButton: "#26373a",
-        secondaryButton: "#3e747b",
-        danger: "#ff6969",
-        success: "#8ce790",
-        warning: "#f0ce7e"
-      },
-      fontFamily: {
-        fugazone: ["Fugaz One", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+	theme: {
+		screens: {
+			sm: "300px",
+			md: "600px",
+			lg: { min: "800px", max: "1279px" },
+			xl: "1280px",
+		},
+		extend: {
+			colors: {
+				primary: "#121b26",
+				secondary: "#1b2834",
+				primaryText: "#ffffff",
+				secondaryText: "#949494",
+				primaryButton: "#26373a",
+				secondaryButton: "#3e747b",
+				danger: "#ff6969",
+				success: "#8ce790",
+				warning: "#f0ce7e",
+			},
+			fontFamily: {
+				fugazone: ["Fugaz One", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
