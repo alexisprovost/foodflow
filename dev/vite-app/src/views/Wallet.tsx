79dff0d7225c8565a63ad2701ac9157aaa1cd596
Alexis Provost <alexis@provost.cloud>
2023-04-20 21:09:14 +0000

    Quick fix

--- dev/vite-app/src/views/Wallet.tsx
+++ dev/vite-app/src/views/Wallet.tsx
@@ -8,20 +8,24 @@ const Wallet = () => {
 	const { isAuthenticated, accessToken } = useContext(AuthContext);
 	useDocumentTitle("Wallet");
 
-	if (isAuthenticated) {
+	if (!isAuthenticated) {
 		return (
 			<div className="animate__animated animate__fadeIn animate__faster">
-				<Title text="Wallet" />
-				<p>Logged in</p>
-			</div>
-		);
-	} else {
-		return (
-			<div className="animate__animated animate__fadeIn animate__faster">
-				<p>Not logged in</p>
+				<div className="my-8">
+					<p>Not logged in</p>
+				</div>
 			</div>
 		);
 	}
+
+	return (
+		<div className="animate__animated animate__fadeIn animate__faster">
+			<Title text="Wallet" />
+			<div className="my-8">
+				<p>Logged in</p>
+			</div>
+		</div>
+	);
 };
 
 export default Wallet;
