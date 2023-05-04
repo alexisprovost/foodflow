619996fb4d1b6ac364a5f159645d12c6bd04c8e1
Alexis Provost <alexis@provost.cloud>
2023-05-04 18:25:13 +0000

    Added account page

--- dev/vite-app/src/views/Account.tsx
+++ dev/vite-app/src/views/Account.tsx
@@ -3,6 +3,8 @@ import useDocumentTitle from "../hooks/useDocumentTitle";
 import Title from "../components/Title";
 import { AuthContext } from "../hooks/AuthProvider";
 
+import Gravatar from "../hooks/Gravatar";
+
 const Account = () => {
 	useDocumentTitle("Account");
 	const { isAuthenticated, userInfo, toggleModal, logout } = useContext(AuthContext);
@@ -15,16 +17,31 @@ const Account = () => {
 			</button>
 		</div>
 	);
+
 	if (isAuthenticated && userInfo) {
-		const { email, id } = userInfo;
+		const { id, firstName, name, email, date_of_birth, role } = userInfo;
 		content = (
-			<div className="animate__animated animate__fadeIn animate__faster flex flex-col justify-center items-center h-[inherit]">
-				<p className="text-xl font-semibold">Logged in</p>
-				<p className="my-2">{email}</p>
-				<p className="text-sm text-gray-500 my-2">{id}</p>
-				<button className="bg-blue-500 text-white py-2 px-4 rounded mt-4" onClick={logout}>
-					Log out
-				</button>
+			<div className="mt-8">
+				<div className="grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-6">
+					<div className="bg-secondary p-8 shadow-md rounded-3xl">
+						<div className="flex flex-col items-center">
+							<Gravatar email={email} size={100} defaultImage="mp" className="rounded-full" />
+							<p className="text-xl font-semibold mt-4">{firstName && name ? firstName + " " + name : email}</p>
+							<button className="bg-blue-500 rounded-xl w-full text-white py-2 px-4 mt-4 hover:bg-blue-500/80" onClick={logout}>
+								Log out
+							</button>
+						</div>
+					</div>
+					<div className="h-3">
+						<div className="rounded-3xl shadow-md cursor-pointer bg-secondary flex flex-col items-center">
+							<h2 className="text-xl w-full py-4 px-6 font-semibold">User Settings</h2>
+							<ul className="w-full">
+								<li className="text-lg font-normal py-4 px-6 hover:bg-black/25 w-full">Orders</li>
+								<li className="text-lg font-normal py-4 px-6 rounded-b-3xl hover:bg-black/25 w-full">Wishlist</li>
+							</ul>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
--- dev/vite-app/src/views/Cart.tsx
+++ dev/vite-app/src/views/Cart.tsx
@@ -1,15 +1,40 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import useDocumentTitle from "../hooks/useDocumentTitle";
 import Title from "../components/Title";
 import CartItem from "../components/CartItem";
 import { CartContext } from "../hooks/CartProvider";
 
+import axios from "axios";
+
 import { Link } from "react-router-dom";
+import { ItemProps } from "../components/StoreItem";
+
+export interface CartItemProps {
+	item: ItemProps;
+	quantity: number;
+	onQuantityChange: (newQuantity: number) => void;
+}
 
 const Cart = () => {
 	useDocumentTitle("Cart");
 	const { getCartItems, addCartItem, removeCartItem, setCartItem } = useContext(CartContext);
 	const cartItems = getCartItems();
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const itemIds = Object.keys(cartItems);
+			const requests = itemIds.map((itemId) => axios.get(`/api/1/products/${itemId}`));
+			try {
+				const responses = await Promise.all(requests);
+				const products = responses.map((response) => response.data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		fetchProducts();
+	}, [cartItems]);
 
 	const increaseQuantity = (itemId: number) => {
 		addCartItem(itemId, 1);
@@ -45,11 +70,12 @@ const Cart = () => {
 			<Title text="Cart" />
 			<div className="mt-8">
 				{Array.isArray(consolidatedItems) && consolidatedItems.length > 0 ? (
-					consolidatedItems.map((item) => {
-						const { id, quantity } = item;
+					consolidatedItems.map((cartItem: CartItemProps) => {
+						const { item, quantity } = cartItem;
+						const { id } = item;
 						return (
-							<div key={id}>
-								<CartItem productId={id} quantity={quantity ?? 0} onQuantityChange={(newQuantity) => handleQuantityChange(id, newQuantity)} />
+							<div key={item.id}>
+								<CartItem item={item} quantity={quantity ?? 0} onQuantityChange={(newQuantity) => handleQuantityChange(id, newQuantity)} />
 							</div>
 						);
 					})
